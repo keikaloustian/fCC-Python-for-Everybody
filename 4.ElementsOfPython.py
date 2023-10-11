111b1627f9a04350e884233a63e50be8372a429d
keikaloustian <kei.kaloustian@gmail.com>
2023-10-10 18:19:34 -0700

    Lesson 4: Elements of Python

---
+++ 4.ElementsOfPython.py
@@ -0,0 +1,39 @@
+# Reserved Words - names that can't be used for variables / identifiers
+
+# False
+# None
+# True
+# and
+# assert
+# break
+# class
+# if
+# def
+# del
+# elif
+# else
+# except
+# return
+# for
+# from
+# global
+# try
+# import
+# in
+# is
+# lambda
+# while
+# not
+# or
+# pass
+# raise
+# finally
+# continue
+# nonlocal
+# with
+# yield
+
+# Sentences or Lines - each statement in Python is written on it's own line; a group of sentences forms a Paragraph
+
+# Interactive (REPL) vs. Script (code in a file)
+
