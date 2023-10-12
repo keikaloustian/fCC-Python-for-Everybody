3e9f6063d3d8e10e0e84a80a9fa09c4f16ba212c
keikaloustian <kei.kaloustian@gmail.com>
2023-10-11 18:22:58 -0700

    Lesson 7: Conditional Execution

---
+++ 07_conditional_execution.py
@@ -0,0 +1,25 @@
+# IF STATEMENT
+x = 5
+if x < 10:
+    print('smaller')
+if x > 10:
+    print('bigger')
+
+# One-line notation
+if x % 2 == 0:  print('even')
+
+# COMPARISON OPERATORS
+# Less than <
+# Less than <
+# Greater than >
+# Greater than or equal >=
+# Equal ==
+# Not equal !=
+
+# IF ELSE STATEMENT
+y = 4
+if y > 2:
+    print('bigger')
+else:
+    print('smaller')
+print('Done')
