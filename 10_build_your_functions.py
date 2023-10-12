e7d78fcd93967204c9184ea54b84834500f89427
keikaloustian <kei.kaloustian@gmail.com>
2023-10-11 21:09:34 -0700

    Lesson 10: Build Your Own Functions

---
+++ 10_build_your_functions.py
@@ -0,0 +1,4 @@
+# PARAMETERS, ARGUMENTS, RETURN VALUES
+# Parameters are variables used in the function definition
+# Arguments are the values that take the place of the parameters when the function is invoked
+# Functions that return a value are called "fruitful", and those that don't are called "void"
