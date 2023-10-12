ddda215e84d7a5873e5753aa75758cbed948d6d1
keikaloustian <kei.kaloustian@gmail.com>
2023-10-11 20:27:47 -0700

    Lesson 9: Python Functions

---
+++ 09_python_functions.py
@@ -0,0 +1,13 @@
+# DEFINING A BASIC FUNCTION
+def greet():
+    print('Hello there')
+
+# Calling the function
+greet()  # -> Hello there
+
+# Python has built-in functions such as print(), int(), type(), float(), etc
+
+
+# MAX & MIN BUILT-IN FUNCTIONS
+max('Hello world')  # -> w (works on strings, in terms of lexicographic order)
+min(1, 19, 44)  #  -> 1
