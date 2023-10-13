03fad2cd51ae833871b1a40f005273b6ccd0b0be
keikaloustian <kei.kaloustian@gmail.com>
2023-10-12 18:10:06 -0700

    Lesson 14: More Patterns

---
+++ 14_more_iteration_patterns.py
@@ -0,0 +1,12 @@
+# COMMON LOOP PATTERNS
+# Counter
+# Total
+# Filter
+# Search  
+
+None  # empty data type
+
+# EQUALITY VS STRICT EQUALITY
+1 == 1.0  # -> True
+1 is 1.0  # -> False
+2 is not '2'  # -> True
