c4ddd9ce54a2e4d1313922b3c23f00195984773d
keikaloustian <kei.kaloustian@gmail.com>
2023-10-12 18:21:49 -0700

    Add equality notes

--- 14_more_iteration_patterns.py
+++ 14_more_iteration_patterns.py
@@ -8,5 +8,8 @@ None  # empty data type
 
 # EQUALITY VS STRICT EQUALITY
 1 == 1.0  # -> True
+1 == '1.0'  # -> False
 1 is 1.0  # -> False
 2 is not '2'  # -> True
+# == / != compares value (but won't convert string to number)
+# is / is not compares value AND type
