faf8f1832616cc201a64ac448ed43ec4156b5ff8
keikaloustian <kei.kaloustian@gmail.com>
2023-10-17 19:57:32 -0700

    Lesson 21: Strings and Lists

---
+++ 21_strings_and_lists.py
@@ -0,0 +1,8 @@
+# SPLIT FUNCTION
+abc = 'With three words'
+abc.split()  # -> ['With', 'three', 'words']
+
+# split() can take an optional delimiter argument
+# By default, it splits at whitespace and treats any number of spaces as just one delimiter
+
+
