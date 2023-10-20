9f458ff4f54f6619fad8c1f29461c700c0e0f2b1
keikaloustian <kei.kaloustian@gmail.com>
2023-10-19 21:10:05 -0700

    Combine Lesson 27 with Lesson 28: Regex: Matching and Extracting Data

---
+++ 27,28_regular_expressions.py
@@ -0,0 +1,31 @@
+# CHARACTER-BASED EXPRESSIONS FOR MATCHING AND PARSING STRINGS
+
+import re  # -> library required
+fh = open('mailbox.txt')
+for line in fh:
+    line = line.rstrip()
+    if re.search('^From:', line):
+        # re.search returns True if reg expression is found in string, otherwise False
+        print(line)
+
+
+# SOME REGEX EXAMPLES
+# ^ matches start of line
+# . any character
+# * signifies "any number of times"
+# \S any non-whitespace character
+# + one or more times
+
+
+# re.findall() - finds and returns list of all matches ([] if none found)
+x = 'My 2 favourite numbers are 11 and 100'
+y = re.findall('[0-9]+', x)  # -> ['2', '11', '100']
+
+
+# GREEDY MATCHING
+# The repeat characters (* and +) will look for the largest possible match by pushing outward in both directions
+
+# To disable greedy matching and match the shortest possible string, follow the * or + with a ?
+
+# The string extraction can be fine-tuned by surrounding the portion of interest with ()
+z = re.findall('^From (\S+@\S+)')  # -> Matching lines need to start with 'From ', but only \S+@\S+ will be returned and assigned to z
