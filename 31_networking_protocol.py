c93fe045aae649a07f12d4efd3b00f949155092e
keikaloustian <kei.kaloustian@gmail.com>
2023-10-21 15:09:19 -0700

    Lesson 31: Networking Protocol

---
+++ 31_networking_protocol.py
@@ -0,0 +1,11 @@
+# APPLICATION PROTOCOLS
+# How do we want to use the reliable sockets built on TCP?
+# What do we want to send across and expect to receive?
+# A protocol is the set of rules governing each connection
+
+
+# HTTP - Hypertext Transfer Protocol
+# Dominant Application Layer Protocol on the internet
+# Created for the web to retrieve HTML, images, documents, etc
+# Extended to handle data in some use cases: RSS, Web Services, etc
+# Basic concept: make connection, request document, receive document, close connection
