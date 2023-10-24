6b977ccc39523a5798e9e159dc518b720eb87631
keikaloustian <kei.kaloustian@gmail.com>
2023-10-23 17:51:07 -0700

    Lesson 36: Using Web Services

---
+++ 36_using_web_services.py
@@ -0,0 +1,4 @@
+# Need for universal formats for the data being transferred to be consumed by different programs, different programming languages.
+# "Wire Protocol" - the format of the data that is actually being sent over the wire, aka the internet.
+# Typical formats are XML, JSON
+
