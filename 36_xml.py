dd49b7fb47a62a392a8e9107f5be8ef9e946f75e
keikaloustian <kei.kaloustian@gmail.com>
2023-10-24 14:07:58 -0700

    Lesson 36: Web Services: XML

---
+++ 36_xml.py
@@ -0,0 +1,28 @@
+# EXTENSIBLE MARKUP LANGUAGE
+# Primary purpose: to help information systems share structured data
+# Syntax is similar to HTML but more strict i.e. there can't be any errors
+# Can be indented for legibility, but it's usually transmitted without whitespace
+
+
+# TAGS
+"""
+    <person>
+        <name>Chuck</name>
+        <email/>
+    </person> 
+"""  
+
+# ATTRIBUTES - key/value pairs inside the opening tag
+"""    <phone type="intl"></phone>
+"""
+
+
+# SERIALIZING / DE-SERIALIZING
+# Converting data from any program/language to and from XML, the common format that can be stored and trasmitted.
+
+
+# XML ELEMENTS / NODES
+# Simple element: tag with no child
+# Complex element: tag with child tags
+# XML can be represented as a tree of nodes or paths; each node can only have one text node within
+
