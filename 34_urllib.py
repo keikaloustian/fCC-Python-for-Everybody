e142b491dc4759c543d50e0cd95336437f5812df
keikaloustian <kei.kaloustian@gmail.com>
2023-10-23 17:25:20 -0700

    Lesson 34: Networking: Using urllib in Python

---
+++ 34_urllib.py
@@ -0,0 +1,8 @@
+# URLLIB - library that does all the socket work and makes web pages read like a file
+
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+for line in fhand:
+    print(line.decode().strip())
