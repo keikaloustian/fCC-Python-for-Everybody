e829170de94e6f7470f54beca100ebc18a31a0a7
keikaloustian <kei.kaloustian@gmail.com>
2023-10-26 20:50:30 -0700

    Lesson 43: Python Objects

---
+++ 43_objects.py
@@ -0,0 +1,12 @@
+# OBJECTS - bits of self-contained code (behaviours) and data
+
+# Everything in Python are objects
+# Objects form boundaries and hide implementation details. The inside and the outside of the objects can be "separated" to make things easier.
+
+
+# DEFINITIONS
+
+# Class - a template
+# Object / Instance - one particular instance of a class. The set of values of the attributes of an object is called its state. The object consists of state and the behavious defined in it's class.
+# Method / Message - a defined capability of a class. Like a function, but scoped to within the objects.
+# Field / Attribute - a piece of data in a class
