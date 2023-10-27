eebb4d0e904ba1613a06c682ce806e64626e667f
keikaloustian <kei.kaloustian@gmail.com>
2023-10-26 20:08:57 -0700

    Lesson 42: Web Services: API Rate Limiting and Security

---
+++ 42_api_rate_limiting.py
@@ -0,0 +1,5 @@
+# RATE LIMITING & SECURITY
+# APIs have costs and often supply valuable information so many providers will limit the rate users can make requests.
+# It is also common for APIs to require accounts and authorization keys.
+# Technologies such as OAuth and cryptography are used to sign request urls with the user secrets / tokens necessary for authorization.
+# The act of appending the authorization information to the request url is known as "augmenting".
