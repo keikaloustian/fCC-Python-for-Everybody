6d058478dca419db7517df8add663b134a85c284
keikaloustian <kei.kaloustian@gmail.com>
2023-10-27 20:24:52 -0700

    Lesson 48: Relational Databases and SQLite

---
+++ 47_relational_databases.py
@@ -0,0 +1,16 @@
+# RELATIONAL DBs
+
+# Storing of data in rows and columns in tables.
+# Have the power to efficiently retrieve data in particular where there are multiple tables and the relationships between the tables are involved in the query.
+
+
+# TERMINOLOGY
+# Database - contains tables
+# Relation / Table - made up of rows and columns
+# Tuple / Row - a set of fields that generally represents an object
+# Attribute / Column / Field - one of possibly many properties assigned to the object in question
+
+
+# STRUCTURED QUERY LANGUAGE (SQL)
+# Standard language for issueing commands to the database.
+# There's SQL that is common to most vendors and products and portions that are unique to each.
