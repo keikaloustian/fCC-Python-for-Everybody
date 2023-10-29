0be358df70cc000e2b157a884ec76eac23761a75
keikaloustian <kei.kaloustian@gmail.com>
2023-10-29 14:53:43 -0700

    Lesson 48: Make a Relational Database

---
+++ 48_relational_db.py
@@ -0,0 +1,4 @@
+# MAKE A RELATIONAL DATABASE WITH SQLITE
+
+# In SQLite, semicolons are statement separators and not terminators.
+# Therefore, they are only required when executing multiple statements in one go. Optional when executing individual commands.
