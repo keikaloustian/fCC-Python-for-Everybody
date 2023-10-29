8304700eb5f55f588f0873e5d52e753026a78f2e
keikaloustian <kei.kaloustian@gmail.com>
2023-10-29 15:25:11 -0700

    Move SQL commands from working sql file to lesson notes

--- 48_relational_db.py
+++ 48_relational_db.py
@@ -2,3 +2,21 @@
 
 # In SQLite, semicolons are statement separators and not terminators.
 # Therefore, they are only required when executing multiple statements in one go. Optional when executing individual commands.
+
+'''
+CREATE TABLE "Users" ("name" TEXT, "email" TEXT)
+
+INSERT INTO Users (name, email) VALUES ('Colleen', 'cvl@umich.edu');
+INSERT INTO Users (name, email) VALUES ('Ted', 'ted@umich.edu');
+INSERT INTO Users (name, email) VALUES ('Sally', 'a1@umich.edu');
+INSERT INTO Users (name, email) VALUES ('Ted', 'ted@umich.edu');
+INSERT INTO Users (name, email) VALUES ('Kristen', 'kf@umich.edu');
+
+DELETE FROM Users WHERE email='ted@umich.edu'
+
+UPDATE Users SET name='Charles' WHERE email='csev@umich.edu'
+
+SELECT * FROM Users WHERE email='cvl@umich.edu'
+
+SELECT * FROM Users ORDER BY email
+'''
