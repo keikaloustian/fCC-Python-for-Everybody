b664500939a2f01b717117148f4d535f89361aa3
keikaloustian <kei.kaloustian@gmail.com>
2023-11-02 14:24:35 -0700

    Lesson 53: Visualizing Data with Python

---
+++ 53_visualizing_data.py
@@ -0,0 +1,6 @@
+# GENERAL DATA ANALYSIS PROCESS
+
+# 1. Data is sourced from somewhere. Could be crawled from the web, could be an API, logs of a web server, etc. For serious/professional data mining, there are technologies available.
+# 2. The raw data is stored in a database. Typically it needs to undergo some processing or cleaning.
+# 3. The cleaned data is stored in a different database. It's typically a smaller subset of the raw data and the processing may have to be done more than once.
+# 4. From there, the cleaned data may be used for the intended analysis and visualization purposes.
